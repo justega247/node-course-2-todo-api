7d5a4d802288509237a7e2e4d7cc95b0aa0076bc
Oghenetega <okeremeta.tega@gmail.com>
2017-12-19 12:42:11 +0100

    Initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,33 @@
+//const MongoClient = require('mongodb').MongoClient;
+
+//same as above,just using es6 destructuring,here you can pull multiple variables out
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').find({_id: new ObjectID('5a38e6539349da1b6db20659')}).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+    
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    db.collection('Users').find({name: 'Jen'}).toArray().then((docs) => {
+        console.log('Users');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch todos', err);
+    });
+
+    //db.close();
+});
